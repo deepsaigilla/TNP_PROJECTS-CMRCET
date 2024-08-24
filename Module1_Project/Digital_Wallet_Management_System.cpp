007367844126ac751df7c8ba02c12542a39dfea3
Deep Sai Gilla <deepsaigilla10@gmail.com>
2024-08-24 11:06:50 +0530

    Update Digital_Wallet_Management_System.cpp
    Adding the code

--- Module1_Project/Digital_Wallet_Management_System.cpp
+++ Module1_Project/Digital_Wallet_Management_System.cpp
@@ -1,1 +1,38 @@
+#include <bits/stdc++.h>
+#include<map>
+using namespace std;
 
+
+int main() {
+    int n;
+    cin>>n;
+    map<int,int>balance;
+    for(int i=0;i<n;i++){
+        int user_id,amt;
+        cin>>user_id>>amt;
+        balance[user_id]=amt;
+    }
+    int q;
+    cin>>q;
+    while(q--){
+        int from,to,amt;
+        cin>>from>>to>>amt;
+        if(amt<=balance[from]){
+            balance[from]-=amt;
+            balance[to]+=amt;
+            cout<<"Success"<<endl;
+        }
+        else{
+            cout<<"Failure"<<endl;
+        }
+    }
+    cout<<"\n";
+    multimap<int,int>sorted;
+    for(auto pair: balance){
+        sorted.insert({pair.second,pair.first});
+    }
+    for(auto pair:sorted){
+        cout<<pair.second<<" "<<pair.first<<endl;
+    }
+    return 0;
+}
